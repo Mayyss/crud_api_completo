d8023fc752d490a1b3403e391063efdf9ad58540
Mayssa <mayssasilva147@gmail.com>
2021-12-08 16:51:40 -0300

    0

---
+++ AULA13/crud/api/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+    //import do arquivo de configuração do sistema
+    require_once("../functions/config.php");
+
+    //Import do arquivo de API de clientes
+    require_once('clientesAPI/index.php');
+
+?>
---
+++ AULA13/crud/bd/listarClientes.php
@@ -0,0 +1,71 @@
+<?php 
+/*************************************************************************
+    Objetivo: Listar todos os dados de Clientes do Banco de Dados
+    Data: 23/09/2021
+    Autor: Marcel
+*************************************************************************/
+//Import do arquivo de conexão com o BD
+require_once(SRC.'bd/conexaoMysql.php');
+
+//retorna todos os registros existentes no banco
+function listar ()
+{
+    $sql = "select tblcliente.*, tblEstado.sigla as uf
+                from tblcliente
+	               inner join tblEstado 
+		           on tblEstado.idEstado = tblCliente.idEstado
+                   order by idcliente desc";
+    
+       
+    //Abre a conexão com o BD
+    $conexao = conexaoMysql();
+    
+    //Solicita aoBD a execução do script SQL
+    $select = mysqli_query($conexao, $sql);
+    
+    return $select;
+    
+}
+
+//retorna apenas um registro, com base no id
+function buscar ($idCliente)
+{
+    $sql = "select tblcliente.*, tblEstado.sigla
+                    from tblcliente
+	               inner join tblEstado 
+		           on tblEstado.idEstado = tblCliente.idEstado
+                where tblcliente.idcliente = ".$idCliente;
+    
+    //Abre a conexão com o BD
+    $conexao = conexaoMysql();
+    
+    
+    //Solicita aoBD a execução do script SQL
+    if ($select = mysqli_query($conexao, $sql))
+        return $select;
+    else
+        return false;
+}
+
+//retorna registros, com base no nome
+function buscarNome ($nome)
+{
+    $sql = "select tblcliente.*, tblEstado.sigla 
+	            from tblcliente
+	               inner join tblEstado 
+		           on tblEstado.idEstado = tblCliente.idEstado
+                where tblcliente.nome like '%".$nome."%'";
+    echo($sql);
+    die;
+    //Abre a conexão com o BD
+    $conexao = conexaoMysql();
+    
+
+    //Solicita aoBD a execução do script SQL
+    if ($select = mysqli_query($conexao, $sql))
+        return $select;
+    else
+        return false;
+}
+
+?>
